4730e9e217dc65339817acea227f90613e8ec211
IlyaBY-afk <ilya.chubryk@gmail.com>
2022-04-02 21:06:32 +0300

    2048 env for agent

--- 2048.py
+++ 2048.py
@@ -1,57 +1,42 @@
-from email.charset import add_charset
-import numpy as np
-from random import choice
+from numpy import array, arange, full, zeros, isin, append
+from numpy.random import choice
+import random
 from itertools import product
+from dataclasses import dataclass
 
 
 def useless_f(x):
-	if x == 0:
-		return 1
+	return x * (x != 0) + (x == 0)
 
-	return x
-
-
+@dataclass
 class Board():
-	def __init__(self, size, final_score) -> None:
-		self.size = size
-		self.final_score = final_score
+	size: int
+	final_score: int
+	score: int = 0
 
-		self.board = np.zeros((size, size), dtype=np.int8)
-		self.blocked = np.full((size, size), False)
-		self.score = 0
-
-	def __repr__(self):
-		result = ''
-
-		for x in np.arange(self.size):
-			new_line = ''
-			for y in np.arange(self.size):
-				new_line = ' | '.join([new_line, str(self.board[y][x])])
-			new_line += '\n' + '---' * (self.size + 2)
-			result += new_line + '\n'
-
-		return result
-
+	def __post_init__(self):
+		self.board: array = zeros((self.size, self.size))
+		self.blocked: array = full((self.size, self.size), False)
 
 	def add_random_tile(self):
 		candidates = []
-		for x, y in product(np.arange(self.size), repeat=2):
-			if self.board[y][x] == 0:
-				candidates.append((y, x))
+		for x, y in product(arange(self.size), repeat=2):
+			if self.board[x][y] == 0:
+				candidates.append((x, y))
 
-		decision = choice(candidates)
-		num = np.random.choice(np.array([2, 4]), p=[0.9, 0.1])
+		decision = random.choice(candidates)
+		num = choice(array([2, 4]), p=[0.9, 0.1])
 		self.board[decision[0]][decision[1]] = num
 		return self.board
 
 	def available_actions(self):
-		actions = np.array([])
+		actions = array([])
 
 		def add_action_x(direction, index, actions):
 			try:
-				if self.board[index][y] == self.board[x][y] or self.board[index][y] == 0:
-					if not np.isin(direction, actions):
-						actions = np.append(actions, direction)
+				if self.board[y][index] in [self.board[y][x], 0]:
+					if not isin(direction, actions):
+						actions = append(actions, direction)
 				return actions
 
 			except IndexError:
@@ -59,16 +44,16 @@ class Board():
 
 		def add_action_y(direction, index, actions):
 			try:
-				if self.board[x][index] == self.board[x][y] or self.board[x][index] == 0:
-					if not np.isin(direction, actions):
-						actions = np.append(actions, direction)
+				if self.board[index][x] in[self.board[y][x], 0]:
+					if not isin(direction, actions):
+						actions = append(actions, direction)
 				return actions
 
 			except IndexError:
 				return actions
 
-		for x, y in product(np.arange(self.size), repeat=2):
-			if self.board[x][y] != 0:
+		for x, y in product(arange(self.size), repeat=2):
+			if self.board[y][x] != 0:
 				actions = add_action_x(direction='LEFT',  index=x-1, actions=actions)
 				actions = add_action_x(direction='RIGHT', index=x+1, actions=actions)
 				actions = add_action_y(direction='UP',    index=y-1, actions=actions)
@@ -89,70 +74,68 @@ class Board():
 
 		k = convert_dict[direction]
 
-		for x, y in product(np.arange(self.size)[::-useless_f(k[0])],
-						    np.arange(self.size)[::-useless_f(k[1])]):
+		for x, y in product(arange(self.size)[::-useless_f(k[0])],
+						    arange(self.size)[::-useless_f(k[1])]):
 
-			if self.board[x][y] != 0:
-				sign = self.board[x][y]
-				self.board[x][y] = 0
+			if self.board[y][x] != 0:
+				sign = self.board[y][x]
+				self.board[y][x] = 0
 
 				if k[0] != 0: 
-					if k[0] == -1:
-						x_range = np.arange(0, x)[::-1]
-					else:
-						x_range = np.arange(x+1, self.size)
+					x_range = arange(0+(x+1)*(k[0]>0), x+(self.size-x)*(k[0]>0))[::k[0]]
 
 					if x_range.size == 0:
-						self.board[x][y] = sign
+						self.board[y][x] = sign
 					
 
 					for x1 in x_range:
-						if self.board[x1][y] == sign and not self.blocked[x1][y]:
-							self.board[x1][y] = sign * 2
+						if self.board[y][x1] == sign and not self.blocked[y][x1]:
+							self.board[y][x1] = sign * 2
 							self.score += sign * 2
-							self.blocked[x1][y] = True
+							self.blocked[y][x1] = True
 							break
-						elif self.board[x1][y] != 0:
-							self.board[x1-k[0]][y] = sign
+						elif self.board[y][x1] != 0:
+							self.board[y][x1-k[0]] = sign
 							break
 					
 						elif x1 == 0 or x1 == self.size - 1:
-							self.board[x1][y] = sign
+							self.board[y][x1] = sign
 							break
 				else:
-					if k[1] == -1:
-						y_range = np.arange(0, y)[::-1]
-					else:
-						y_range = np.arange(y+1, self.size)
+					y_range = arange(0+(y+1)*(k[1]>0), y+(self.size-y)*(k[1]>0))[::k[1]]
 
 					if y_range.size == 0:
-						self.board[x][y] = sign
+						self.board[y][x] = sign
 
 					for y1 in y_range:
-						if self.board[x][y1] == sign and not self.blocked[x][y1]:
-							self.board[x][y1] = sign * 2
+						if self.board[y1][x] == sign and not self.blocked[y1][x]:
+							self.board[y1][x] = sign * 2
 							self.score += sign * 2
-							self.blocked[x][y1] = True
+							self.blocked[y1][x] = True
 							break
-						elif self.board[x][y1] != 0:
-							self.board[x][y1-k[1]] = sign
+						elif self.board[y1][x] != 0:
+							self.board[y1-k[1]][x] = sign
 							break
 						elif y1 == 0 or y1 == self.size - 1:
-							self.board[x][y1] = sign
+							self.board[y1][x] = sign
 							break
-		self.blocked = np.full((self.size, self.size), False)
+
+		self.blocked[self.blocked==True] = False
 
 
 def run():
 	board = Board(4, 2048)
 	board.add_random_tile()
-	score = 0
 
 	while True:
-		print(board)
+		print(board.board)
 
 		options = board.available_actions()
-		if options.size == 0:
+		if board.score == board.final_score:
+			print('You won!')
+			break
+
+		elif options.size == 0:
 			print('Game Over! No moves available!')
 			break
 
@@ -160,7 +143,7 @@ def run():
 		print(f'Available moves: {options}')
 		
 		while True:
-			next_move = input('Input your next move: ')
+			next_move = input('It your next move: ')
 			if next_move in options:
 				break
 		
@@ -169,6 +152,3 @@ def run():
 
 
 run()
-
-
-			
